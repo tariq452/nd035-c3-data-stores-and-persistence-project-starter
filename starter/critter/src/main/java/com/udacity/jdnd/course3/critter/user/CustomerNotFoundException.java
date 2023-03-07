69a1c2b1d428bee8e00eece959e93987e1c911bc
tariqtami <tariq.a.tami@gmail.com>
2023-03-08 00:28:49 +0300

    add new classes

---
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/CustomerNotFoundException.java
@@ -0,0 +1,15 @@
+package com.udacity.jdnd.course3.critter.user;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+public class CustomerNotFoundException extends RuntimeException {
+
+    public CustomerNotFoundException() {
+    }
+
+    public CustomerNotFoundException(String message) {
+        super(message);
+    }
+}
